491b17af813269a0d9d8ebef81846e4c500005c6
InzamamRahaman <izzyrahaman@gmail.com>
2019-03-22 23:56:52 -0400

    Added some changes

--- htorch/fuck.py
+++
@@ -1,9 +0,0 @@
-import torch as th
-import nn
-import numpy as np
-import operations
-
-inputs = th.rand((5, 3), requires_grad=True)
-linear = nn.HyperbolicLinearLinear(3, 2, False)
-
-linear(inputs)
